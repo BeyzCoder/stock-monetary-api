d531f20b36de6bcc2b3be4c50dd60465fc89fc99
BeyzCoder <stevenbaes11@yahoo.com>
2025-09-06 16:07:25 -0600

    Added Traefik, TLS, healthcheck.

--- app/api/v1/db/database.py
+++ app/api/v1/db/database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.exc import OperationalError
 from app.api.v1.config import DB_CONFIG
 
 engine = create_engine("postgresql://{}:{}@{}:{}/{}".format(*DB_CONFIG))
